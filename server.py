573fa1922570ad20a01221aaa21b30c46b399e9b
Shafahatou Adamou <shafahatouadamou@gmail.com>
2024-09-23 02:50:36 +0200

    bug/tableau-des-points-pour-eviter-le-favoritisme

--- server.py
+++ server.py
@@ -95,3 +95,9 @@ if __name__ == '__main__':
     # run() method of Flask class runs the application 
     # on the local development server.
     app.run()
+
+
+@app.route('/points')
+def showPoints():
+    return render_template('points.html', clubs=clubs)
+
